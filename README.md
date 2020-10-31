cd2afa1d46034cf1c699e1a6fb6d6beceda9e9a4
MOHQ <73744329+MOHQ-TM@users.noreply.github.com>
2020-10-31 19:40:34 +0330

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Admin_Panel_Finder
-A tool to find the admin panel of sites Written by Python.
+# MOHQ Security Team
+A tool to find the admin panel.
+
+## Install and run
+bash
+git clone https://github.com/MOHQ-TM/APF
+cd APF
+python3 admin_f.py
+
+## Video Tutorial
+
+
+## Contact us
+- Telegram Channel : [@Hamiyan_Haj_Qassem](https://t.me/Hamiyan_Haj_Qassem)
+- Gmail : [mohq.team@gmail.com](mailto:mohq.team@gmail.com)
+- Github : [MOHQ-TM](https://github.com/MOHQ-TM)
---
+++ admin_finder.py
@@ -0,0 +1,73 @@
+import requests as r
+import os
+
+
+class color:
+    GREEN = '\033[92m'
+    GREEN1 = '\033[32m'
+    WHITE = '\033[0m'
+    RED = '\033[91m'
+    BLUE = '\033[96m'
+
+
+def __start__():
+
+    os.system('clear')
+	
+	
+    print(color.GREEN1 + '')
+	
+    print('███╗   ███╗ ██████╗ ██╗  ██╗ ██████╗     ████████╗███╗   ███╗')
+    print('████╗ ████║██╔═══██╗██║  ██║██╔═══██╗    ╚══██╔══╝████╗ ████║')
+    print('██╔████╔██║██║   ██║███████║██║   ██║       ██║   ██╔████╔██║')
+    print('██║╚██╔╝██║██║   ██║██╔══██║██║▄▄ ██║       ██║   ██║╚██╔╝██║')
+    print('██║ ╚═╝ ██║╚██████╔╝██║  ██║╚██████╔╝       ██║   ██║ ╚═╝ ██║')
+    print('╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚══▀▀═╝        ╚═╝   ╚═╝     ╚═╝')
+    print(color.GREEN + '                            ************************************')
+    print('                            ** Telegram : @Hamiyan_Haj_Qassem **')
+    print('                            **     Email : mohq@gmail.com     **')
+    print('                            **      Developer : PDHNMSN       **')
+    print('                            ************************************')
+
+
+    #url
+    url = str(input (color.BLUE+"Enter your target address (Example: www.google.com) : "))
+    
+    #admin_list
+    ad_list=open("admin.txt","r")
+    
+    #range
+    if "http://" in url :
+    
+        ulr = (url + "/")
+        
+    elif "https://" in url :
+    
+        url = (url + "/")
+        
+    else :
+    
+        url = ("http://" + url + "/")
+
+
+
+
+    for i in ad_list:
+    
+        req=r.post(url+i.strip())
+        
+        if req.status_code == 200:
+        
+            print("{} Admin Panel {} => {} {} {} Found".format(color.GREEN,color.RED,color.GREEN1,url+i,color.GREEN))
+            
+            f=open("result.txt","a+")
+            
+            f.write(str(url+i))
+            
+
+        else:
+        
+            print("{} Not Found {} {}".format(color.RED,color.WHITE,url+i))
+            
+            
+__start__()
