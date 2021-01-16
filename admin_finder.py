549b7d27ee99b9e5477c9dbe9d5094ab3a51c9b4
MOHQ <73744329+MOHQ-TM@users.noreply.github.com>
2021-01-16 16:41:50 -0500

    Update admin_finder.py

--- admin_finder.py
+++ admin_finder.py
@@ -3,70 +3,73 @@ import os
 
 
 class color:
-    GREEN = '\033[92m'
-    GREEN1 = '\033[32m'
-    WHITE = '\033[0m'
-    RED = '\033[91m'
-    BLUE = '\033[96m'
+	GREEN = '\033[92m'
+	GREEN1 = '\033[32m'
+	WHITE = '\033[0m'
+	RED = '\033[91m'
+	BLUE = '\033[96m'
 
 
-def __start__():
+try:
 
-    os.system('clear')
+	os.system('clear')
 	
 	
-    print(color.GREEN1 + '')
+	print(color.GREEN1 + '')
 	
-    print('███╗   ███╗ ██████╗ ██╗  ██╗ ██████╗     ████████╗███╗   ███╗')
-    print('████╗ ████║██╔═══██╗██║  ██║██╔═══██╗    ╚══██╔══╝████╗ ████║')
-    print('██╔████╔██║██║   ██║███████║██║   ██║       ██║   ██╔████╔██║')
-    print('██║╚██╔╝██║██║   ██║██╔══██║██║▄▄ ██║       ██║   ██║╚██╔╝██║')
-    print('██║ ╚═╝ ██║╚██████╔╝██║  ██║╚██████╔╝       ██║   ██║ ╚═╝ ██║')
-    print('╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚══▀▀═╝        ╚═╝   ╚═╝     ╚═╝')
-    print(color.GREEN + '                            ************************************')
-    print('                            ** Telegram : @MOHQ_Team **')
-    print('                            **     Email : mohq@gmail.com     **')
-    print('                            ************************************')
+	print('███╗   ███╗ ██████╗ ██╗  ██╗ ██████╗     ████████╗███╗   ███╗')
+	print('████╗ ████║██╔═══██╗██║  ██║██╔═══██╗    ╚══██╔══╝████╗ ████║')
+	print('██╔████╔██║██║   ██║███████║██║   ██║       ██║   ██╔████╔██║')
+	print('██║╚██╔╝██║██║   ██║██╔══██║██║▄▄ ██║       ██║   ██║╚██╔╝██║')
+	print('██║ ╚═╝ ██║╚██████╔╝██║  ██║╚██████╔╝       ██║   ██║ ╚═╝ ██║')
+	print('╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚══▀▀═╝        ╚═╝   ╚═╝     ╚═╝')
+	print(color.GREEN + '                            ************************************')
+	print('                            ** Telegram : @MOHQ_Team **')
+	print('                            **     Email : mohq@gmail.com     **')
+	print('                            ************************************')
+	print('ctrl+c for exit!')
 
 
-    #url
-    url = str(input (color.BLUE+"Enter your target address (Example: www.google.com) : "))
+	#url
+	url = str(input (color.BLUE+"Enter your target address (Example: www.google.com) : "))
     
-    #admin_list
-    ad_list=open("admin.txt","r")
+	#admin_list
+	ad_list=open("admin.txt","r")
     
-    #range
-    if "http://" in url :
+	#range
+	if "http://" in url :
     
-        ulr = (url + "/")
+		ulr = (url + "/")
         
-    elif "https://" in url :
+	elif "https://" in url :
     
-        url = (url + "/")
+		url = (url + "/")
         
-    else :
+	else :
     
-        url = ("https://" + url + "/")
+		url = ("https://" + url + "/")
 
 
 
 
-    for i in ad_list:
+	for i in ad_list:
     
-        req=r.post(url+i.strip())
+		req=r.post(url+i.strip())
         
-        if req.status_code == 200:
+	if req.status_code == 200:
         
-            print("{} Admin Panel {} => {} {} {} Found".format(color.GREEN,color.RED,color.GREEN1,url+i,color.GREEN))
+		print("{} Admin Panel {} => {} {} {} Found".format(color.GREEN,color.RED,color.GREEN1,url+i,color.GREEN))
             
-            f=open("result.txt","a+")
+		f=open("result.txt","a+")
             
-            f.write(str(url+i))
+		f.write(str(url+i))
             
 
-        else:
+	else:
         
-            print("{} Not Found {} {}".format(color.RED,color.WHITE,url+i))
+		print("{} Not Found {} {}".format(color.RED,color.WHITE,url+i))
             
             
-__start__()
+except :
+	os.system('clear')
+	print (color.BLUE + "\nOK goodbye !!!")
